c7f56a25744da0be09a7bab472098874ef95ee9f
Ekopratama <Ekopratama55@gmail.com>
2016-10-23 20:19:09 +0700

    Latihan2_1

---
+++ Selasa_141110574_EkoPratamaCendana/Latihan2_1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Latihan2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Kalender = new System.Windows.Forms.MonthCalendar();
+            this.Tanggal = new System.Windows.Forms.NumericUpDown();
+            this.Bulan = new System.Windows.Forms.DomainUpDown();
+            this.Tambah = new System.Windows.Forms.Button();
+            this.Hapus = new System.Windows.Forms.Button();
+            this.Lahir = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Tanggal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Kalender
+            // 
+            this.Kalender.Location = new System.Drawing.Point(18, 18);
+            this.Kalender.Name = "Kalender";
+            this.Kalender.TabIndex = 4;
+            // 
+            // Tanggal
+            // 
+            this.Tanggal.Location = new System.Drawing.Point(257, 18);
+            this.Tanggal.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.Tanggal.Name = "Tanggal";
+            this.Tanggal.Size = new System.Drawing.Size(120, 20);
+            this.Tanggal.TabIndex = 5;
+            this.Tanggal.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Bulan
+            // 
+            this.Bulan.Location = new System.Drawing.Point(386, 18);
+            this.Bulan.Name = "Bulan";
+            this.Bulan.Size = new System.Drawing.Size(120, 20);
+            this.Bulan.TabIndex = 1;
+            this.Bulan.Text = "Bulan";
+            // 
+            // Tambah
+            // 
+            this.Tambah.Location = new System.Drawing.Point(257, 53);
+            this.Tambah.Name = "Tambah";
+            this.Tambah.Size = new System.Drawing.Size(120, 23);
+            this.Tambah.TabIndex = 2;
+            this.Tambah.Text = "Tambah";
+            this.Tambah.UseVisualStyleBackColor = true;
+            this.Tambah.Click += new System.EventHandler(this.Tambah_Click);
+            // 
+            // Hapus
+            // 
+            this.Hapus.Location = new System.Drawing.Point(386, 53);
+            this.Hapus.Name = "Hapus";
+            this.Hapus.Size = new System.Drawing.Size(120, 23);
+            this.Hapus.TabIndex = 3;
+            this.Hapus.Text = "Hapus";
+            this.Hapus.UseVisualStyleBackColor = true;
+            this.Hapus.Click += new System.EventHandler(this.Hapus_Click);
+            // 
+            // Lahir
+            // 
+            this.Lahir.AutoSize = true;
+            this.Lahir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lahir.Location = new System.Drawing.Point(258, 94);
+            this.Lahir.Name = "Lahir";
+            this.Lahir.Size = new System.Drawing.Size(173, 19);
+            this.Lahir.TabIndex = 5;
+            this.Lahir.Text = "Tanggal Lahir : 10 Oktober";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 261);
+            this.Controls.Add(this.Lahir);
+            this.Controls.Add(this.Hapus);
+            this.Controls.Add(this.Tambah);
+            this.Controls.Add(this.Bulan);
+            this.Controls.Add(this.Tanggal);
+            this.Controls.Add(this.Kalender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Tanggal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar Kalender;
+        private System.Windows.Forms.NumericUpDown Tanggal;
+        private System.Windows.Forms.DomainUpDown Bulan;
+        private System.Windows.Forms.Button Tambah;
+        private System.Windows.Forms.Button Hapus;
+        private System.Windows.Forms.Label Lahir;
+    }
+}
+
---
+++ Selasa_141110574_EkoPratamaCendana/Latihan2_1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan2_1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> dmonth = new Dictionary<string, int>();
+        string smonth = "";
+        DateTime date;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            date = new DateTime(DateTime.Now.Year, 1, DateTime.Now.Day);
+            for (int i = 1; i <= 12; i++)
+            {
+                smonth = date.ToString("MMMM");
+                Bulan.Items.Add(smonth);
+                dmonth.Add(smonth, i);
+                date = date.AddMonths(1);
+            }
+            for (DateTime i = new DateTime(2016, 1, 1); i.Year <= 2017; i = i.AddDays(1))
+            {
+                if (i.DayOfWeek == DayOfWeek.Saturday || i.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    Kalender.AddBoldedDate(i);
+                }
+            }
+            Kalender.AddAnnuallyBoldedDate(new DateTime(2016, 12, 28));
+            Kalender.UpdateBoldedDates();
+        }
+
+        private void Tambah_Click(object sender, EventArgs e)
+        {
+            BoldDateRange("Tambah");
+            if (Bulan.Text != "Month")
+            {
+                Kalender.AddAnnuallyBoldedDate(
+                    new DateTime(DateTime.Now.Year, dmonth[Bulan.Text], Convert.ToInt32(Tanggal.Value)));
+                Kalender.UpdateBoldedDates();
+            }
+        }
+
+        private void Hapus_Click(object sender, EventArgs e)
+        {
+            BoldDateRange("Hapus");
+            if (Bulan.Text != "Month")
+            {
+                Kalender.RemoveAnnuallyBoldedDate(
+                    new DateTime(DateTime.Now.Year, dmonth[Bulan.Text], Convert.ToInt32(Tanggal.Value)));
+                Kalender.UpdateBoldedDates();
+            }
+        }
+
+        private void BoldDateRange(string action)
+        {
+            for (DateTime i = Kalender.SelectionStart.Date; i <= Kalender.SelectionEnd.Date; i = i.AddDays(1))
+            {
+                if (action == "Tambah")
+                {
+                    Kalender.AddBoldedDate(i);
+                }
+                else if (action == "Hapus")
+                {
+                    Kalender.RemoveBoldedDate(i);
+                }
+            }
+            Kalender.UpdateBoldedDates(); action = "";
+        }
+    }
+}
