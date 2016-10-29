51081251e90478a29ec57917ac73db73d17c2c5b
Ekopratama <Ekopratama55@gmail.com>
2016-10-29 20:47:38 +0700

    Latihan3_1

---
+++ Selasa_141110574_EkoPratamaCendana/Latihan 3_1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Latihan_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.ukuran = new System.Windows.Forms.ToolStripComboBox();
+            this.tebal = new System.Windows.Forms.ToolStripButton();
+            this.miring = new System.Windows.Forms.ToolStripButton();
+            this.garis = new System.Windows.Forms.ToolStripButton();
+            this.jenis = new System.Windows.Forms.ToolStripComboBox();
+            this.warna = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ukuran,
+            this.tebal,
+            this.miring,
+            this.garis,
+            this.jenis,
+            this.warna});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(530, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(0, 28);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(530, 382);
+            this.rtb1.TabIndex = 1;
+            this.rtb1.Text = "";
+            // 
+            // ukuran
+            // 
+            this.ukuran.Name = "ukuran";
+            this.ukuran.Size = new System.Drawing.Size(121, 28);
+            // 
+            // tebal
+            // 
+            this.tebal.CheckOnClick = true;
+            this.tebal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tebal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tebal.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tebal.Name = "tebal";
+            this.tebal.Size = new System.Drawing.Size(24, 25);
+            this.tebal.Text = "B";
+            // 
+            // miring
+            // 
+            this.miring.CheckOnClick = true;
+            this.miring.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.miring.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miring.Image = ((System.Drawing.Image)(resources.GetObject("miring.Image")));
+            this.miring.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miring.Name = "miring";
+            this.miring.Size = new System.Drawing.Size(23, 25);
+            this.miring.Text = "I";
+            // 
+            // garis
+            // 
+            this.garis.CheckOnClick = true;
+            this.garis.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.garis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.garis.Image = ((System.Drawing.Image)(resources.GetObject("garis.Image")));
+            this.garis.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.garis.Name = "garis";
+            this.garis.Size = new System.Drawing.Size(25, 25);
+            this.garis.Text = "U";
+            // 
+            // jenis
+            // 
+            this.jenis.Name = "jenis";
+            this.jenis.Size = new System.Drawing.Size(121, 28);
+            // 
+            // warna
+            // 
+            this.warna.Name = "warna";
+            this.warna.Size = new System.Drawing.Size(121, 28);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 422);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox ukuran;
+        private System.Windows.Forms.ToolStripButton tebal;
+        private System.Windows.Forms.ToolStripButton miring;
+        private System.Windows.Forms.ToolStripButton garis;
+        private System.Windows.Forms.ToolStripComboBox jenis;
+        private System.Windows.Forms.ToolStripComboBox warna;
+        private System.Windows.Forms.RichTextBox rtb1;
+    }
+}
+
---
+++ Selasa_141110574_EkoPratamaCendana/Latihan 3_1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Latihan_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void jenis_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.Graphics.DrawString(jenis.Items[e.Index].ToString(), new Font(jenis.Items[e.Index].ToString(), jenis.Font.Size), Brushes.Black, e.Bounds);
+        }
+
+        private void warna_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index >= 0)
+            {
+                Graphics g = e.Graphics;
+                Brush bb = new SolidBrush(e.BackColor);
+                Brush bf = new SolidBrush(e.ForeColor);
+
+                g.FillRectangle(bb, e.Bounds);
+                string s = (string)this.warna.Items[e.Index].ToString();
+                SolidBrush b = new SolidBrush(Color.FromName(s));
+                e.Graphics.DrawRectangle(Pens.Black, 2, e.Bounds.Top + 1, 20, 11);
+                e.Graphics.FillRectangle(b, 3, e.Bounds.Top + 2, 19, 10);
+                e.Graphics.DrawString(s, this.Font, Brushes.Black, 25, e.Bounds.Top);
+                bb.Dispose();
+                bf.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection arr = new InstalledFontCollection();
+            foreach (FontFamily i in arr.Families)
+            {
+                jenis.Items.Add(i.Name);
+            }
+            jenis.SelectedIndex = 14;
+
+            for (int i = 5; i <= 72; i++)
+                ukuran.Items.Add(i);
+            ukuran.SelectedIndex = 7;
+
+            jenis.ComboBox.LostFocus += new EventHandler(jenis_LostFocus);
+            ukuran.ComboBox.LostFocus += new EventHandler(jenis_LostFocus);
+            jenis.ComboBox.DrawItem += new DrawItemEventHandler(jenis_DrawItem);
+            jenis.ComboBox.DrawMode = DrawMode.OwnerDrawFixed;
+
+            foreach (PropertyInfo property in typeof(Color).GetProperties())
+            {
+                if (property.PropertyType == typeof(Color))
+                    warna.Items.Add(property.Name);
+            }
+
+            warna.SelectedIndex = 8;
+            warna.ComboBox.LostFocus += new EventHandler(jenis_LostFocus);
+            warna.ComboBox.DrawItem += new DrawItemEventHandler(warna_DrawItem);
+            warna.ComboBox.DrawMode = DrawMode.OwnerDrawFixed;
+
+            berubah();
+        }
+        public void berubah()
+        {
+            float z;
+            if (ukuran.Text == "")
+                z = 12;
+            else
+                z = (float)Convert.ToDouble(ukuran.SelectedItem);
+            //FontStyle a = new FontStyle();
+            //if (tebal.Checked)
+            //{
+            //    a = FontStyle.Bold;
+            //    if (miring.Checked)
+            //        a = FontStyle.Italic;
+            //    else if (garis.Checked)
+            //        a = FontStyle.Underline;
+            //}
+            //else if (miring.Checked)
+            //{
+            //    a = FontStyle.Italic;
+            //    if (tebal.Checked)
+            //        a = FontStyle.Bold;
+            //    else if (garis.Checked)
+            //        a = FontStyle.Underline;
+            //}
+            //else if (garis.Checked)
+            //{
+            //    a = FontStyle.Underline;
+            //    if (tebal.Checked)
+            //        a = FontStyle.Bold;
+            //    else if (miring.Checked)
+            //        a = FontStyle.Italic;
+            //}
+            //else
+            //    a = FontStyle.Regular;
+            FontStyle a = (tebal.Checked) ? FontStyle.Bold : FontStyle.Regular;
+            a |= (miring.Checked) ? FontStyle.Italic : FontStyle.Regular;
+            a |= (garis.Checked) ? FontStyle.Underline : FontStyle.Regular;
+            Font baru = new Font(jenis.SelectedItem.ToString(), z, a);
+            rtb1.SelectionFont = baru;
+            rtb1.SelectionColor = Color.FromName(warna.Text);
+            rtb1.Focus();
+        }
+
+        private void jenis_LostFocus(object sender, EventArgs e)
+        {
+            berubah();
+        }
+
+        private void rtb1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (rtb1.SelectionFont != null)
+            {
+                try
+                {
+                    jenis.SelectedItem = rtb1.SelectionFont.FontFamily.Name;
+                }
+                catch
+                {
+                    jenis.Text = "";
+                }
+
+                try
+                {
+                    ukuran.SelectedIndex = (int)rtb1.SelectionFont.Size - 5;
+                }
+                catch { ukuran.Text = ""; }
+
+                try
+                {
+                    warna.SelectedItem = rtb1.SelectionColor.Name;
+                }
+                catch
+                {
+                    warna.Text = "";
+                }
+
+                if (rtb1.SelectionFont.Style.ToString().IndexOf("Bold") != -1)
+                    tebal.Checked = true;
+
+                if (rtb1.SelectionFont.Style.ToString().IndexOf("Italic") != -1)
+                    miring.Checked = true;
+
+                if (rtb1.SelectionFont.Style.ToString().IndexOf("Underline") != -1)
+                    garis.Checked = true;
+            }
+            else
+            {
+                ukuran.SelectedIndex = 7;
+                jenis.SelectedIndex = 14;
+            }
+        }
+
+        private void ukuran_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox x = (ToolStripComboBox)sender;
+            if (!x.Focused)
+                return;
+            berubah();
+        }
+
+        private void tebal_Click(object sender, EventArgs e)
+        {
+            berubah();
+        }
+
+        private void miring_Click(object sender, EventArgs e)
+        {
+            berubah();
+        }
+
+        private void garis_Click(object sender, EventArgs e)
+        {
+            berubah();
+        }
+
+        private void jenis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox x = (ToolStripComboBox)sender;
+            if (!x.Focused)
+                return;
+            berubah();
+        }
+    }
+}
